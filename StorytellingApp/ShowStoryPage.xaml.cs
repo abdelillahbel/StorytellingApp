ffb9bdbdeda41330544c859477242bf40c194f61
abdelillahbel <abdelillahbel@engineer.com>
2024-05-16 03:55:16 +0100

    Update UI/UX & add story speaking feature

--- StorytellingApp/ShowStoryPage.xaml.cs
+++ StorytellingApp/ShowStoryPage.xaml.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Resources;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace StorytellingApp
 {
@@ -18,6 +12,44 @@ namespace StorytellingApp
 
             Story = story;
             BindingContext = this;
+        }
+
+        CancellationTokenSource cts;
+
+        private async void OnSpeakButtonClicked(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            var locales = await TextToSpeech.GetLocalesAsync();
+            var locale =
+                locales.FirstOrDefault(l => l.Country.Equals("AR", StringComparison.InvariantCultureIgnoreCase));
+
+            var settings = new SpeechOptions()
+            {
+                Pitch = 1.0f, // 0.0 - 2.0
+                Volume = 0.75f, // 0.0 - 1.0
+                Locale = locale
+            };
+
+            await TextToSpeech.SpeakAsync(Story.Content!, settings, cancelToken: cts.Token);
+        }
+
+        private void OnPauseButtonClicked(object sender, EventArgs e)
+        {
+            // Pause functionality is not available in Xamarin.Essentials.TextToSpeech
+        }
+
+        private void OnStopButtonClicked(object sender, EventArgs e)
+        {
+            CancelSpeech();
+            // Stop functionality is not available in Xamarin.Essentials.TextToSpeech
+        }
+
+        private void CancelSpeech()
+        {
+            if (cts?.IsCancellationRequested ?? true)
+                return;
+
+            cts.Cancel();
         }
     }
 }
